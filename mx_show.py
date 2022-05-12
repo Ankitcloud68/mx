8645aaeb146eee472b97e6b1b16b9dfd76b5b245
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-12 13:44:10 +0530

    Update mx_show.py

--- mx_show.py
+++ mx_show.py
@@ -10,11 +10,12 @@ scraper = cloudscraper.create_scraper()
 a14=input("Enter Mx player url here: ")
 a1=scraper.get(a14).text
 soup4=BeautifulSoup(a1,'html.parser')
+print(soup4)
 with open('s86.txt','w',encoding='utf-8')as s4:
     for i in soup4.prettify():
         s4.write(i)
 a11=soup4.find_all("script",type="application/ld+json")
+print(a11)
 outer = re.compile("show/watch-dhahanam-tamil/season-1/")
 a82=outer.search(str(a11))
-a41=a82.groups(0)
-print(41)
+print(a82)
