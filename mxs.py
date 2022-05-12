6f290566f1bbfe92e3aa9f186c11574beddba6b1
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-12 17:31:05 +0530

    Update mxs.py

--- mxs.py
+++ mxs.py
@@ -2,10 +2,6 @@ import requests
 import cloudscraper
 from bs4 import BeautifulSoup
 import json
-import re
-import m3u8
-import os
-global a71
 scraper = cloudscraper.create_scraper()
 a14=input("Enter Mx player url here: ")
 a1=scraper.get(a14).text
