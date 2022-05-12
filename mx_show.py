fc6a1eeb8630b66035b81e099a6e1845dddc01a9
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-12 11:44:44 +0530

    Update mx_show.py

--- mx_show.py
+++ mx_show.py
@@ -8,8 +8,8 @@ import os
 global a71
 scraper = cloudscraper.create_scraper()
 a14=input("Enter Mx player url here: ")
-a111=x[6]
 a1=scraper.get(a14).text
 soup4=BeautifulSoup(a1,'html.parser')
 inputs = soup4.find_all('episode-',type="webUrl")
-print (inputs[0])
+x="https://mxplayer.in"+inputs[0]
+print(x)
