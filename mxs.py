1a9d15cd4c5363132e1aea51e6a353c8e77cf870
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-12 17:09:52 +0530

    Update mxs.py

--- mxs.py
+++ mxs.py
@@ -10,10 +10,7 @@ scraper = cloudscraper.create_scraper()
 a14=input("Enter Mx player url here: ")
 a1=scraper.get(a14).text
 soup4=BeautifulSoup(a1,'html.parser')
-with open('s86.txt','w',encoding='utf-8')as s4:
-    for i in soup4.prettify():
-        s4.write(i)
 a11=soup4.find_all("div",{"class":"slide"})
 for i in a11:
- y=i.find("a","href")
+ y=i.find("href")
  print(y)
