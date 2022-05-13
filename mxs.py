8f57391ed174c3cd82c2187185702a7ff63073d2
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-13 14:06:28 +0530

    Update mxs.py

--- mxs.py
+++ mxs.py
@@ -4,15 +4,11 @@ from bs4 import BeautifulSoup
 import json
 import re
 
-scraper = cloudscraper.create_scraper(allow_brotli=False)
+scraper = cloudscraper.create_scraper(delay=10)
 a14=input("Enter Mx player url here: ")
 a1=scraper.get(a14).text
-print ("SEE THIS \n",a1)
-print ("\n")
 soup4=BeautifulSoup(a1,'html.parser')
-print("\nIDHA PAARU : \n",soup4)
 a11=soup4.find_all("div",{"class":"slide"})
-print("\n",a11)
 a123=re.compile('/show/[-\w]+/[-\w]+/[-\w]+-[-\w]+')
 for i in a11:
  j=str(i)
