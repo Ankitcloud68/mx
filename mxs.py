4cb4ec383dee110e8960582abbffe358124d0cc0
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-13 12:36:18 +0530

    Update mxs.py

--- mxs.py
+++ mxs.py
@@ -7,6 +7,7 @@ a14=input("Enter Mx player url here: ")
 a1=scraper.get(a14).text
 soup4=BeautifulSoup(a1,'html.parser')
 a11=soup4.find_all("div",{"class":"slide"})
-print(a11[0])
-
-
+a123=re.compile('/show/[-\w]+/[-\w]+/[-\w]+-[-\w]+')
+for i in a11:
+ i=a123.findall(a11)
+ print(i)
